92c27bc9c9430add575e5eefc6a30d94c80642c5
Sara Srivastav <jhilmil1995@gmail.com>
2019-07-25 00:14:52 -0400

    added aria lables and fix css when displaying resturant details

--- js/main.js
+++ js/main.js
@@ -174,6 +174,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.setAttribute('alt', restaurant.name);
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
